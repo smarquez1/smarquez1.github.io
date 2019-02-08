17b8a14d0120e51fe788114d4cd2762d9cc66799
Sergio D. Márquez <marquez.sergio.d@gmail.com>
2019-02-08 10:13:17 +0100

    Updates

--- src/scripts/main.js
+++
@@ -1,7 +0,0 @@
-const feather = require('feather-icons')
-import { CopyText } from './utils'
-
-feather.replace()
-
-document.querySelector('#gpgIcon')
-  .addEventListener('click', () => CopyText('#gpg'))
