7fede875e79ea849af1213d39a009584ec7160bb
Sergio D. Márquez <marquez.sergio.d@gmail.com>
2019-02-27 12:34:40 +0100

    Modified bg/fg vars

--- dist/utils.js
+++
@@ -1,6 +0,0 @@
-export function CopyText(query) {
-  console.log(query)
-  const copyText = document.querySelector(query)
-  copyText.select()
-  document.execCommand('copy')
-}
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,3 @@
 const feather = require('feather-icons')
-import { CopyText } from './utils'
 
 feather.replace()
-
-document.querySelector('#gpgIcon')
-  .addEventListener('click', () => CopyText('#gpg'))
